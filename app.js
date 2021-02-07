aa8ea0ef6d404f9df540350b37f2d31f2297c80b
Daniel2711 <detaddeo.d@gmail.com>
2021-02-07 23:03:01 +0100

    Inserimento ordinati dischi secondo l'anno d'uscita

--- app.js
+++ app.js
@@ -12,15 +12,20 @@ new Vue({
 
         axios
             .get('https://flynn.boolean.careers/exercises/api/array/music')
+            
             .then(dataAPI =>{
                 console.log(dataAPI.data.response);
                 this.dischiMusicali = dataAPI.data.response;
+
+                // ordinamento per valore
+                this.dischiMusicali = this.dischiMusicali.sort(function (a, b) {
+                    return a.year - b.year;
+                });
             })
             .catch(error =>{
                 console.log('Error in the API call');
                 console.log(error);
             })
-        
     },
 
     methods: {
