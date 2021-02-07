b80934272607fc73269222b52e23af1bb3d4ad78
Daniel2711 <detaddeo.d@gmail.com>
2021-02-07 22:07:50 +0100

    Add cdn axios

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 new Vue({
     el:'#app',
+
     data:{
         test: 'test'
-    }
+    },
+
+    beforeCreate() {
+        this.callAPI();
+    },
+    
+    methods: {
+        callAPI: function(){
+            console.log('Call API')
+        }
+    },
 })
