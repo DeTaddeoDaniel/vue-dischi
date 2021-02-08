8883b3ea8c3c8aeb70379e56611dffee1e81b5f5
Daniel2711 <detaddeo.d@gmail.com>
2021-02-08 14:12:38 +0100

    Inserita ricerca title o autore degli album

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ new Vue({
     data:{
         test: 'test',
         selected: "All",
+        textSearch: "",
+        selectedSearch: "Author",
         dischiMusicali :undefined,
     },
 
@@ -40,24 +42,63 @@ new Vue({
 
     methods: {
 
+        // cancella testo input
+        resetInputSearch: function(){
+            this.textSearch = '';
+            this.filtrer();
+        },
+
         // filtra dischi per genere musicali
         filtrer: function(){
 
             this.dischiMusicali.forEach( disco =>{
                 
-                // no search
+                //search generi musicali
                 if( this.selected != 'All' ){
+                    
+                    // controlla il genere dell'album
                     if(this.selected == disco.genre){
                         disco.visibility = true;
                     } else {
                         disco.visibility = false;
                     }
-                
-                // no search
+
                 } else {
                     disco.visibility = true;
                 }
+
+                // search generi musicali
+                let search = this.textSearch.toLowerCase();
+                let typeSearch = this.selectedSearch.toLowerCase();
+                let title = disco.title.toLowerCase();
+                let autore= disco.author.toLowerCase();
+
+                // entra se testo diverso da stringa vuota e disco visibile dopo filtraggio genere
+                if( search != "" && disco.visibility){
+                    
+                    // controlla autore
+                    if(typeSearch == 'author'){
+                        
+                        if(autore.includes(search)){
+                            disco.visibility = true;
+                        } else {
+                            disco.visibility = false;
+                        }
+
+                    // controlla titolo album
+                    } else if(typeSearch == 'title'){
+                        
+                        if(title.includes(search)){
+                            disco.visibility = true;
+                        } else {
+                            disco.visibility = false;
+                        }
+
+                    }
+                }
             })
-        }
+        },
+
+
     },
 })
